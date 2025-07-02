70d57d325e74ac4d632bc84863b9342b6c4c89c2
Md Muqtadir Fuad <mmfuad01@gmail.com>
2025-07-03 01:29:21 +0600

    Previous repo file deleted

--- footer.php
+++
@@ -1,3 +0,0 @@
-<footer>
-    <p>&copy; 2025 My Modern Webpage. All rights reserved.</p>
-</footer>
--- scripts.js
+++
@@ -1,5 +0,0 @@
-// Basic JavaScript Example: Alert on form submission
-document.querySelector('form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Thank you for your message!');
-});
